5b72c69380486d530cac62b30a13f4e7696d463f
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-13 19:20:32 -0800

    Les fondamentaux de javascipt

---
+++ js1/js/main.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log(typeof "Raymond");
+console.log(typeof "30");
+console.log(typeof true);
---
+++ random_game/main.js
@@ -0,0 +1,5 @@
+// My first code challenge
+
+//Write a code that will return a random letter from my name
+
+console.log("Raymond".charAt())
