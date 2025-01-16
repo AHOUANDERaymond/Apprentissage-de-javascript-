d235ae6ed36dc376d070aef8ec851148dca79725
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-15 18:00:01 -0800

    Init Commit

---
+++ js3-3/js/main.js
@@ -0,0 +1,13 @@
+//Numbers
+
+//An integer is a whole number.
+const myNumber=42;
+const myFloat= 42.0353667;
+const myString= "42";
+
+
+//A number with a decimal point is a float which references the"floating point"
+
+
+
+console.log(Number.parseFloat("4.237abc").toFixed(2).toString());
